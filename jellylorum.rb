11bf4836e4b8da539beadab4604ebdcb9ce1d9a9
xiongchiamiov <xiong.chiamiov@gmail.com>
2010-02-22 23:22:53 -0800

    add really hacky pseudo-inheritance for templates, and DRY things up a bit, too

--- jellylorum.rb
+++ jellylorum.rb
@@ -4,8 +4,19 @@ require 'liquid'
 
 $file_system = Liquid::LocalFileSystem.new("views")
 $templates = {}
-$templates['index'] = $file_system.read_template_file("index")
+['base', 'index'].each {|template| $templates[template] = $file_system.read_template_file(template)}
+
+helpers do
+	def render(template, data)
+		Liquid::Template.parse($templates['base'])
+						.render('content' => Liquid::Template.parse($templates[template])
+															.render(data))
+	end
+end
 
 get '/' do
-	Liquid::Template.parse($templates['index']).render
+	data = {}
+	data['message'] = 'Hihi~'
+	
+	render('index', data)
 end
