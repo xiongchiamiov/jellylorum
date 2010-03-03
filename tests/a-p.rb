b04582e15c7896c00b93fa7c04ba2588b899164f
xiongchiamiov <xiong.chiamiov@gmail.com>
2010-03-03 01:16:00 -0800

    tests: don't do unnecessary webscrapes/api calls

--- tests/a-p.rb
+++ tests/a-p.rb
@@ -11,24 +11,26 @@ class APTest < Test::Unit::TestCase
 			@knt.update!
 		end
 		
-		should 'have the correct type' do
-			assert_equal 'TV', @knt.type
-		end
-		
-		should 'have the correct number of episodes' do
-			assert_equal '20+', @knt.episodeCount
-		end
-		
-		should 'have the correct year started' do
-			assert_equal 2009, @knt.startDate.year
-		end
-		
-		should 'have the correct year ended' do
-			assert_equal nil, @knt.endDate
-		end
-		
-		should 'have the correct description' do
-			assert_equal "Sawako Kuronuma is just like any other high school girl who wants to make friends and be useful. The only problem is she bears a worrying resemblance to Sadako from 'The Ring!' Because of her reputation, people are not only terrified of her, but small dogs even bark in fear at her presence; in fact, the only person in school who will talk to her is the lively class hottie, Kazehara. As the pair spends more time together, Kazehara slowly begins to bring Sawako out of her shell and soon their feelings for each other develop further. Though with her crippling insecurities, lack of social skills, and a series of cruel rumors and misunderstandings, it seems that Sawako's dream of a normal life won’t be quite so easy to obtain.", @knt.description
+		context 'updated from the web' do
+			should 'have the correct type' do
+				assert_equal 'TV', @knt.type
+			end
+			
+			should 'have the correct number of episodes' do
+				assert_equal '20+', @knt.episodeCount
+			end
+			
+			should 'have the correct year started' do
+				assert_equal 2009, @knt.startDate.year
+			end
+			
+			should 'have the correct year ended' do
+				assert_equal nil, @knt.endDate
+			end
+			
+			should 'have the correct description' do
+				assert_equal "Sawako Kuronuma is just like any other high school girl who wants to make friends and be useful. The only problem is she bears a worrying resemblance to Sadako from 'The Ring!' Because of her reputation, people are not only terrified of her, but small dogs even bark in fear at her presence; in fact, the only person in school who will talk to her is the lively class hottie, Kazehara. As the pair spends more time together, Kazehara slowly begins to bring Sawako out of her shell and soon their feelings for each other develop further. Though with her crippling insecurities, lack of social skills, and a series of cruel rumors and misunderstandings, it seems that Sawako's dream of a normal life won’t be quite so easy to obtain.", @knt.description
+			end
 		end
 	end
 end
