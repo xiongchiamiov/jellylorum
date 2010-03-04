9885977d1087fe8a1fbd753e0769ee8a336c33f7
xiongchiamiov <xiong.chiamiov@gmail.com>
2010-03-04 04:03:55 -0800

    ditch liquid for haml (RAAAAAAAAGE)

--- jellylorum.rb
+++ jellylorum.rb
@@ -1,31 +1,49 @@
 # -*- coding: UTF-8 -*-
 require 'rubygems'
 require 'sinatra'
-require 'liquid'
+require 'haml'
 require 'dm-core'
+
+require 'models/anime'
 require 'models/anidb'
+require 'models/layout'
 
 # setup stuff
 DataMapper::Logger.new($stdout, :debug)
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/sqlite.db")
 DataMapper::setup(:mongodb, :adapter => 'mongo', :database => 'jellylorum')
-DataMapper.auto_migrate!
-
-$file_system = Liquid::LocalFileSystem.new("views")
-$templates = {}
-['base', 'index'].each {|template| $templates[template] = $file_system.read_template_file(template)}
-
-helpers do
-	def render(template, data)
-		Liquid::Template.parse($templates['base'])
-						.render('content' => Liquid::Template.parse($templates[template])
-															.render(data))
-	end
-end
+AniDB::Anime.auto_migrate!
 
 get '/' do
 	data = {}
 	data['message'] = 'Hihi~'
 	
-	render('index', data)
+	haml :index, :locals => data
+end
+
+post '/anime/' do
+	redirect '/anime/kimi-ni-todoke/'
+end
+
+get '/anime/*' do
+	data = {}
+	
+	anime = repository(:mongodb) { Anime.create }
+	anime.anidb = 6466
+	
+	data['anime'] = {'anidb' => AniDB::Anime.new(6466, 'tests/knt.xml') }
+	data['anime']['anidb'].update!
+	
+	layout = repository(:mongodb) { Layout.create }
+	layout.columns = [[20,
+					   [['anidb', 'type'],
+						['anidb', 'episodeCount']]],
+					  [50,
+					   [['anidb', 'description']]],
+					  [29,
+					   [['anidb', 'startDate'],
+						['anidb', 'endDate']]]]
+	data['layout'] = layout
+	
+	haml :anime, :locals => data
 end
