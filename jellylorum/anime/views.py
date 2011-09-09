ce26e5a1ff76a46fa042d29b50a89ce10738fb74
xiongchiamiov <xiong.chiamiov@gmail.com>
2011-09-09 04:41:08 -0700

    *really* basic view

--- jellylorum/anime/views.py
+++ jellylorum/anime/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from anime.models import Anime, AP, AniDB, ANN
+from django.shortcuts import render_to_response
+
+def details(request, slug):
+	data = {}
+	data['anime'] = Anime.objects.get(slug=slug)
+	return render_to_response('anime/details.html', data)
+
--- jellylorum/urls.py
+++ jellylorum/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import redirect_to
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -8,6 +9,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'jellylorum.views.home', name='home'),
     # url(r'^jellylorum/', include('jellylorum.foo.urls')),
+	url(r'^$', redirect_to, {'url': '/dia/'}),
+	url(r'^dia/anime/(?P<slug>[\w-]+)/$', 'anime.views.details'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
