4f063a9cf185cd9387c99df49b856f4eeb518c98
xiongchiamiov <xiong.chiamiov@gmail.com>
2012-07-08 23:10:36 -0700

    display list of animes on /anime/

--- jellylorum/urls.py
+++ jellylorum/urls.py
@@ -1,6 +1,9 @@
 from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic import ListView
 from django.views.generic.simple import redirect_to
+
+from anime.models import Anime
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -11,6 +14,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'jellylorum.views.home', name='home'),
     # url(r'^jellylorum/', include('jellylorum.foo.urls')),
 	url(r'^$', redirect_to, {'url': '/dia/'}),
+	url(r'^dia/anime/$', ListView.as_view(model=Anime)),
 	url(r'^dia/anime/(?P<slug>[\w-]+)/$', 'anime.views.details'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
