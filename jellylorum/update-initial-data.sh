dd092481e1f875f1c69bb5e31bd4c6d51eb8a31c
xiongchiamiov <xiong.chiamiov@gmail.com>
2012-10-27 13:17:12 -0700

    add initial data update script

---
+++ jellylorum/update-initial-data.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -ex
+
+./manage.py shell <<<"from anime.models import *
+knt = AniDB.objects.get(pk=6466)
+knt.update()
+knt.save()"
+./manage.py dumpdata anime --indent=4 > anime/initial_data.json
+git diff anime/initial_data.json
+
