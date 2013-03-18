08b8af30029759caf490113ea2fd7bea9ef543d5
xiongchiamiov <xiong.chiamiov@gmail.com>
2013-03-18 00:30:51 -0700

    Consistencize indentation.

--- jellylorum/urls.py
+++ jellylorum/urls.py
@@ -10,23 +10,23 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'jellylorum.views.home', name='home'),
-    # url(r'^jellylorum/', include('jellylorum.foo.urls')),
+	# Examples:
+	# url(r'^$', 'jellylorum.views.home', name='home'),
+	# url(r'^jellylorum/', include('jellylorum.foo.urls')),
 	url(r'^$', redirect_to, {'url': '/dia/'}),
 	url(r'^dia/anime/$', ListView.as_view(model=Anime)),
 	url(r'^dia/anime/(?P<slug>[\w-]+)/$', 'anime.views.details'),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+	# Uncomment the admin/doc line below to enable admin documentation:
+	url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+	# Uncomment the next line to enable the admin:
+	url(r'^admin/', include(admin.site.urls)),
 )
 
 if settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes':True}),
+	urlpatterns += patterns('',
+		(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes':True}),
 )
 
 
